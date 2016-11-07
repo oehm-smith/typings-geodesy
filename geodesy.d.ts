f6f5100b8e592058aec90b6b97201e52f97c697d
Brooke Smith <brooke.smith@ga.gov.au>
2016-11-07 14:35:34 +1100

    Cant use . (dots) in types.

--- geodesy.d.ts
+++ geodesy.d.ts
@@ -1,17 +1,17 @@
-interface GML_3_2_1.CodeType {
+interface GML_3_2_1_CodeType {
     codeSpace: string,
     value: string
 }
 
-interface GML_3_2_1.StringOrRefType {
+interface GML_3_2_1_StringOrRefType {
     value: array<string>
 }
 
-interface GML_3_2_1.TimePositionType {
+interface GML_3_2_1_TimePositionType {
     value: array<string>
 }
 
-interface GML_3_2_1.ReferenceType {
+interface GML_3_2_1_ReferenceType {
     owns: boolean,
     href: string
 }
@@ -25,16 +25,16 @@ interface HumiditySensor {
     manufacturer: string,  
     serialNumber: string,
     heightDiffToAntenna: number,
-    calibrationDate: GML_3_2_1.TimePositionType,
-    validTime: GML_3_2_1.TimePrimitivePropertyType,
+    calibrationDate: GML_3_2_1_TimePositionType,
+    validTime: GML_3_2_1_TimePrimitivePropertyType,
     # SensorType
-    type: GML_3_2_1.CodeType
+    type: GML_3_2_1s_.CodeType
     # AbstractGMLType
-    description: GML_3_2_1.StringOrRefType
-    descriptionReference: GML_3_2_1.ReferenceType
+    description: GML_3_2_1_StringOrRefType
+    descriptionReference: GML_3_2_1_ReferenceType
     # Other (don't know)
     extension: any
-    identifier: GML_3_2_1.CodeType
+    identifier: GML_3_2_1_CodeType
     remarks: string
 }
 
